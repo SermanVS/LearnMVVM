1f229a17ab9fe2074211ca85ed954fe63ec5e96a
Sergei Tikhomirov <sergey.zr.co.uk@gmail.com>
2020-09-13 00:52:42 +0300

    Intermediate

---
+++ LearnMVVM/Model/ItemModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnMVVM.Model
+{
+    class ItemModel
+    {
+        private int value;
+       
+        public ItemModel(int _value = 0)
+        {
+            this.setValue(_value);
+        }
+
+        public int getValue()
+        {
+            return value;
+        }
+        public void setValue(int _value)
+        {
+            this.value = _value;
+        }
+        
+        public ItemModel getAsSumOf(ItemModel valueA, ItemModel valueB)
+        {
+            this.setValue(valueA.getValue() + valueB.getValue());
+            return this;
+        }
+    }
+}
---
+++ LearnMVVM/ViewModel/BinarySummatorViewModel.cs
@@ -0,0 +1,53 @@
+﻿using LearnMVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace LearnMVVM.ViewModel
+{
+    class BinarySummatorViewModel : INotifyPropertyChanged
+    {
+        ItemModel itemA = new ItemModel();
+        ItemModel itemB = new ItemModel();
+        ItemModel result = new ItemModel();
+        public BinarySummatorViewModel()
+        {
+            GetResCommand = new Command(GetResult);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        public void GetResult()
+        {
+            result.getAsSumOf(itemA, itemB);
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(DisplayMessage));
+        }
+
+        public int ItemAValue
+        {
+            get { return itemA.getValue(); }
+            set { itemA.setValue(value); }
+        }
+
+        public int ItemBValue
+        {
+            get { return itemB.getValue(); }
+            set { itemB.setValue(value); }
+        }
+
+        public string DisplayMessage
+        {
+            get { return $"The sum of A and B is: {result.getValue()}"; }
+        }
+
+        public Command GetResCommand { get; }
+    }
+}
