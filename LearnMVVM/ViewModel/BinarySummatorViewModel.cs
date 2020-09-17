9938921ff67c9d03996410f4eeaffd2271754c15
Sergei Tikhomirov <sergey.zr.co.uk@gmail.com>
2020-09-17 23:22:55 +0300

    Fixed project structure

--- LearnMVVM/ViewModel/BinarySummatorViewModel.cs
+++ LearnMVVM/ViewModel/BinarySummatorViewModel.cs
@@ -10,7 +10,7 @@ namespace LearnMVVM.ViewModel
 {
     class BinarySummatorViewModel : INotifyPropertyChanged
     {
-        ItemModel itemModel = new ItemModel();
+        private SummatorModel summatorModel = new SummatorModel();
         public BinarySummatorViewModel()
         {
             ComputeResultCommand = new Command(ComputeResult);
@@ -24,26 +24,31 @@ namespace LearnMVVM.ViewModel
         }
         public void ComputeResult()
         {
-            itemModel.setResultAsSumOf(itemModel.itemA, itemModel.itemB);
+            summatorModel.setResultAsSum();
             OnPropertyChanged();
             OnPropertyChanged(nameof(DisplayMessage));
         }
 
-        public int ItemAValue
+        public int itemA
         {
-            get { return itemModel.itemA.getValue(); }
-            set { itemModel.itemA.setValue(value); }
+            get { return summatorModel._itemA.getValue(); }
+            set { summatorModel._itemA.setValue(value); }
         }
 
-        public int ItemBValue
+        public int itemB
         {
-            get { return itemModel.itemB.getValue(); }
-            set { itemModel.itemB.setValue(value); }
+            get { return summatorModel._itemB.getValue(); }
+            set { summatorModel._itemB.setValue(value); }
+        }
+
+        public int result
+        {
+            get { return summatorModel._result.getValue(); }
         }
 
         public string DisplayMessage
         {
-            get { return $"The sum of A and B is: {itemModel.result.getValue()}"; }
+            get { return $"The sum of A and B is: { result }"; }
         }
 
         public Command ComputeResultCommand { get; }
