8732c795049e719dd8ba4da5792d76bb615cf336
Sergei Tikhomirov <sergey.zr.co.uk@gmail.com>
2020-09-17 23:23:42 +0300

    Reorganized classes

---
+++ LearnMVVM/Model/SummatorModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearnMVVM.Model
+{
+    class SummatorModel
+    {
+        public Item _itemA;
+        public Item _itemB;
+        public Item _result;
+
+        public SummatorModel(int valueA = 0, int valueB = 0)
+        {
+            _itemA = new Item(valueA);
+            _itemB = new Item(valueB);
+            _result = new Item(valueA + valueB);
+        }
+        public void setResultAsSum()
+        {
+            _result.setValue(_itemA.getValue() + _itemB.getValue());
+        }
+    }
+
+    class Item
+    {
+        private int value;
+
+        public Item(int _value = 0)
+        {
+            this.setValue(_value);
+        }
+
+        public int getValue()   
+        {
+            return value;
+        }
+        public void setValue(int _value)
+        {
+            this.value = _value;
+        }
+    }
+}
